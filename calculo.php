1ac594a16979868e5ecfb356d1965b3fe32ad522
alanN99 <alannanni99@gmail.com>
2021-09-10 20:09:55 -0300

    Index y php con calculo y muestra de resultados en pantalla

---
+++ calculo.php
@@ -0,0 +1,20 @@
+<?php   
+use function CommonMark\Render\HTML;
+
+$num1 = $_GET['num1'];
+$num2 = $_GET['num2'];
+
+$suma = $num1 + $num2;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Resultados:</h1>
+    <?php echo "<p>La suma de $num1 + $num2 = $suma</p>";?>
+</body>
+</html>
