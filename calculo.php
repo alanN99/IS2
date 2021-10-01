0cf8afadc244f3ff1a2ee697923cba57a7cf8d0d
milagrosN13 <90486190+milagrosN13@users.noreply.github.com>
2021-10-01 13:11:17 -0300

    agregar las operaciones restantes

--- calculo.php
+++ calculo.php
@@ -6,7 +6,13 @@ $num2 = $_GET['num2'];
 $suma = $num1 + $num2;
 $resta = $num1 - $num2;
 $producto = $num1 * $num2;
+$exponencia = $n1 ** $n2;
 
+if ($num2 == 0){
+    $division = "No se puede dividir entre 0";
+}else{
+    $division = $num1 / $num2;
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,8 +25,10 @@ $producto = $num1 * $num2;
 <body>
     <h1>Resultados:</h1>
     <?php echo "<p>La suma de $num1 + $num2 = $suma</p>";
-            echo "<p>La resta de $num1 - $num2 = $resta</p>";
-          echo "<p>El producto de $num1 * $num2 = $producto</p>";  
+          echo "<p>La resta de $num1 - $num2 = $resta</p>";
+          echo "<p>El producto de $num1 * $num2 = $producto</p>";
+          echo "<p>El cociente de $num1 / $num2 = $division</p>";
+          echo "<p>La exponenciación de  $num1 elevado a $num2 = $exponencia</p>";
     ?>
 </body>
 </html>
